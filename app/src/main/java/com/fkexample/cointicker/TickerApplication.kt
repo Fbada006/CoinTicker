90b45dd4c7e73b8fff89b271b35ffe95e1fad403
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2023-06-24 01:49:15 +0300

    set up timber for logging

--- app/src/main/java/com/fkexample/cointicker/TickerApplication.kt
+++ app/src/main/java/com/fkexample/cointicker/TickerApplication.kt
@@ -2,6 +2,19 @@ package com.fkexample.cointicker
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class TickerApplication: Application()
+class TickerApplication: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
